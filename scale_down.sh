9f9f43ef60ed071ffd7a660506d52159c383ff4c
nicholbs <nicholbs@stud.ntnu.no>
2021-11-09 11:13:13 +0100

    første utkast av infrastruktur

---
+++ scale_down.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -v
+
+#sjekk om fil med scale up/down URL eksisterer
+FILE=~/infrastructureAsCode/manager/scale_down_url
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else
+    echo "$FILE does NOT exist"
+    exit 1
+fi
+
+#Hent scale up/down URL
+scale_up_url=$(cat ~/integrasjonsprosjekt/openStack/manager/scale_down_url)
+
+#Send POST forespørsel om å minske antall servere til openstack API-et 
+curl -X POST $scale_up_url
+
